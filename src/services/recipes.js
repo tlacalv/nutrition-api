49865673d781b865a913ce93fa89e78ee9c16b3b
Tlacaelel <radeon777@live.com>
2021-01-08 09:24:58 -0700

    Changed word in recipe methods

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -6,36 +6,36 @@ class RecipesService {
         this.mongoDB = new MongoLib();
     }
   // db.getCollection('ingredients').find({$text: { $search: "Tomato jalapeño"}},{score: { $meta: "textScore"}}).sort({score: {$meta: "textScore"}})
-    async getIngredients () {
+    async getRecipes () {
         const recipes = await this.mongoDB.getAll(this.collection, {})
         return recipes || []
     }
-    async getUserIngredients ({userId}) {
+    async getUserRecipes({userId}) {
         const query = userId && {userId}
         const recipes = await this.mongoDB.getAll(this.collection, query)
         return recipes || []
     }
-    async searchIngredient ({text}) {
+    async searchRecipe({text}) {
         const query = { $text: {$search: text}, score: { $meta: "textScore"}}
         const sort = {score: {$meta: "textScore"}}
         const recipes = await this.mongoDB.search(this.collection, query, sort)
         return recipes || []
     }
-    async getIngredient ({ recipeId }) {
+    async getRecipe ({ recipeId }) {
         
         const recipe = await this.mongoDB.get(this.collection, recipeId);
         return recipe;
     }
 
-    async createIngredient({ recipe }) {
+    async createRecipe({ recipe }) {
         const createdRecipe = await this.mongoDB.create(this.collection, recipe)
         return createdRecipe;
     }
-    async updateIngredient ({recipeId, recipe}) {
+    async updateRecipe ({recipeId, recipe}) {
         const updateRecipeId = await this.mongoDB.update(this.collection, recipeId, recipe)
         return updateRecipeId
     }
-    async deleteIngredient({recipeId}) {
+    async deleteRecipe({recipeId}) {
         const deletedRecipeId = await this.mongoDB.delete(this.collection, recipeId)
         return deletedRecipeId
     }
