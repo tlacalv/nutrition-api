4e16d12d61165f44ffe7e02b4cc577528d68b9ed
Tlacaelel <radeon777@live.com>
2021-01-22 10:07:36 -0700

    Changed mongo string to connect to atlas

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -6,7 +6,7 @@ const PASSWORD = encodeURIComponent(config.dbPassword)
 const DB_NAME = config.dbName;
 const debug = require("debug")("app:db");
 
-const MONGO_URI = `mongodb://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`;
 
 class MongoLib {
   constructor() {
