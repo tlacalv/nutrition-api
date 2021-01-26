9951d2c67fa9e336b5eb84d73ce0e75255deacae
Tlacaelel <radeon777@live.com>
2021-01-26 08:16:08 -0700

    Added helmet

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 const express = require("express")
 const app = express()
 const {errorHandler, wrapErrors, logErrors} = require('./src/utils/middleware/errorHandlers')
-const jwt = require("jsonwebtoken")
+const helmet = require('helmet')
 const boom = require('@hapi/boom')
 const authRoutes = require('./src/routes/auth')
 const ingredientsRoutes = require('./src/routes/ingredients')
 const recipesRoutes = require('./src/routes/recipes')
 const usersRoutes = require('./src/routes/users')
 
-
+//helmet
+app.use(helmet())
 
 app.use(express.json())
 //errors
